65ce3270f53560bd4e60d6da442a9700ccd3339f
WairenD <WairenD@users.noreply.github.com>
2019-11-28 01:18:42 +0100

    Robert Murcsek
    added lib.php , cleaned up code

--- register.php
+++ register.php
@@ -12,45 +12,19 @@
           <form method="post">
               <div><a href="login.php"><img id="backbtn" src="img/back.png" alt="back"></a></div>
               <img id="logo" src="img/nhl.png" alt="nhl">
+              <input type="text" name="firstname" placeholder="First Name">
+              <input type="text" name="lastname" placeholder="Last Name">
               <input class="mail" type="email" name="email" placeholder="Email">
               <input type="password" name="pw" placeholder="Password">
               <input type="password" name="pwr" placeholder="Repeat Password">
               <div id="errorDiv">
               <?php
-                $existingUsers = file("users.csv");
-                  if(isset($_POST['register'])){
-                    if(!empty($_POST['email']) && !empty($_POST['pw']) && !empty($_POST['pwr'])){
-                      if($_POST['pw'] != $_POST['pwr']){
-                        echo "<p>Passwords do not match!</p>";
-                      }else{
-                        $inputEmail=$_POST['email'];
-                        $inputPW=password_hash($_POST['pw'], PASSWORD_DEFAULT);
-                        if(sizeof($existingUsers)>0){
-                        foreach ($existingUsers as $email) { //checks if the user is already registered
-                          $email = explode(",",$email);
-                          if($email[0] == $inputEmail){
-                            echo "<p>Email already registered!</p>";
-                            break;
-                          }else{
-                            $userData = fopen("users.csv","a");
-                            $temp = array($inputEmail,$inputPW);
-                            fputcsv($userData, $temp);
-                            fclose($userData);
-                            echo "<p class='success'>Email succesfully registered!</p>";
-                            break;
-                          }
-                        }
-                      }
-                      else{
-                        $userData = fopen("users.csv","a");
-                        $temp = array($inputEmail,$inputPW);
-                        fputcsv($userData, $temp);
-                        fclose($userData);
-                        echo "<p class='success'>Email succesfully registered!</p>";
-                      }
-                    }
+                include("lib.php");
+                if(isset($_POST['register'])){
+                  if(!empty($_POST['email']) && !empty($_POST['pw']) && !empty($_POST['pwr']) && !empty($_POST['firstname']) && !empty($_POST['lastname'])){
+                  registerUser($_POST['firstname'],$_POST['lastname'],$_POST['email'],$_POST['pw'],$_POST['pwr']);
                   }else{
-                  echo "<p>Please fill in your details!</p>";
+                    echo "<p>Please fill in your details!</p>";
                   }
                 }
                ?>
