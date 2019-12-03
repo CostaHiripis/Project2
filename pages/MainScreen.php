ab1988c527a20733869e6032c6e6fec482249a4c
KBG360 <KBG360@users.noreply.github.com>
2019-12-03 13:49:10 +0100

    Konstantin Boguev
    Box model for now

---
+++ pages/MainScreen.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="MainScreenCss.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Stenden HelpDesk</title>
+</head>
+
+<body>
+    <div id="MainScreen">
+        <div id="header">
+            <div id="profilepicbox">
+                <p id="username">$username</p>
+                <div id="stendenlogo">
+                </div>
+</body>
+
+</html>
