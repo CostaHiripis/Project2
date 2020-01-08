a4d5da9d8be72e542f2660e9c9ada554d460b02e
KBG360 <55129891+KBG360@users.noreply.github.com>
2020-01-08 13:44:15 +0100

    Konstantin Boguev

---
+++ pages/mainmenu.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Stenden HelpDesk</title>
+    <link rel="stylesheet" type="text/css" href="../CSS/mainmenu.css">
+</head>
+
+<body>
+    <div id="fullPage">
+        <div id="header">
+            <img id="logoPic" src="../img/nhl.png" alt="nhl">
+            <img id="userPic" src="../img/defuserpic.png" alt="userPic">
+        </div>
+
+        <div id="main">
+
+            <div id="spacer-top"></div>
+
+            <div id="maincontent">
+
+                <div id="left-complex">
+                    <div id="top-left-complex">
+                        <div id="rectangle-div">
+                            <a href="#">
+                                <div class="effect" id="effectblue"></div>
+                            </a>
+                        </div>
+
+                        <div id="square-div">
+                            <a href="#">
+                                <div class="effect" id="effectlblue"></div>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div id="bottom-left-complex">
+                        <div id="square-bottom-div">
+                            <a href="#">
+                                <div class="effect" id="effectlblue"></div>
+                            </a>
+                        </div>
+
+                        <div id="rectangle-bottom-div">
+                            <a href="#">
+                                <div class="effect" id="effectblue"></div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div id="right-complex">
+                    <div class="bigBox">
+                        <a href="#">
+                            <div class="effect" id="effectblue"></div>
+                        </a>
+                    </div>
+
+                </div>
+
+                <div id="spacer-bottom"></div>
+
+            </div>
+
+        </div>
+</body>
+
+</html>
+
+<!-- <div id="boxWide">
+                    <a href="#">
+                        <div class="effect" id="effectblue">
+
+                        </div>
+                </div> -->
