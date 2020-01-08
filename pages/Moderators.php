23f30ee66c0aa2b79c4681f40783a340f83b2933
presentmichael <57666995+presentmichael@users.noreply.github.com>
2020-01-08 14:09:25 +0100

    2 new files one css and one contact moderator file,
    css file is new and specific to the Moderators.php file which only has html in it

---
+++ pages/Moderators.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Stenden HelpDesk</title>
+        <link rel="stylesheet" href="../CSS/moderatorscreen.css">
+    </head>
+    <body>
+        <div id="fullPage">
+            <div id="header">
+                <img id="logoPic" src="../img/nhl.png" alt="nhl">
+                <img id="userPic" src="../img/Jesus.jpg" alt="userPic">
+            </div>
+            <div class="list" id="effectblue">
+                <div class="TicketsHeader" id="effectteal">
+                    <h1>Moderators</h1>
+                </div>
+		
+		<div class="mod" id="effectlblue">
+                    <a href="mailto:raymond.blankestijn@nhlstenden.com">
+                    <p>Name: </p>
+                    <p>Email:</p> 
+                    </a>
+		</div>
+                                   
+		<div class="mod" id="effectlblue">
+                    <a href="mailto:raymond.blankestijn@nhlstenden.com">    
+                    <p>Name:</p>
+                    <p>Email:</p>
+                    </a>    
+		</div>
+                
+		<div class="mod" id="effectlblue">
+                    <a href="mailto:raymond.blankestijn@nhlstenden.com">    
+                    <p>Name:</p>
+                    <p>Email:</p>
+                    </a>
+		</div>				
+                
+		<div class="mod" id="effectlblue">
+                    <a href="mailto:raymond.blankestijn@nhlstenden.com">
+                    <p>Name:</p>
+                    <p>Email:</p>
+                    </a>
+		</div>	
+                
+		<div class="mod" id="effectlblue">
+                    <a href="mailto:raymond.blankestijn@nhlstenden.com">
+                    <p>Name:</p>
+                    <p>Email:</p> 
+                    </a>
+		</div>
+                    
+		<div class="mod" id="effectlblue">
+                    <a href="mailto:raymond.blankestijn@nhlstenden.com">
+                    <p>Name:</p>
+                    <p>Email:</p> 
+                    </a>  
+		</div>
+            </div>
+        </div>
+    </body>
+</html>
