98e09c22bb7713e1a41e9a9bde3b19a72fa96a30
KBG360 <55129891+KBG360@users.noreply.github.com>
2020-01-08 13:55:28 +0100

    Revert "Merge branch 'develop' of https://github.com/WairenD/Project-2 into develop"
    This reverts commit 7a202ea5b90d5a00e7121bfeac0dcf13991d5343, reversing
    changes made to a4d5da9d8be72e542f2660e9c9ada554d460b02e.

--- pages/adminTickets.php
+++ pages/adminTickets.php
@@ -17,56 +17,17 @@
                 <div class="TicketsHeader" id="effectteal">
                     <h3>Open Tickets</h3>
                 </div>
-                
-                <?php
-                include 'connect.php';
-
-                $SQLString = "SELECT * FROM " . $TicketTable;
-
-                if ($stmt = mysqli_prepare($DBConnect, $SQLString)) {
-                    mysqli_stmt_execute($stmt);
-                    mysqli_stmt_bind_result($stmt, $TicketId, $Title, $OpenDat, $CloseDate, $Status, $UserId, $AdminId, $Type);
-                    mysqli_stmt_store_result($stmt);
-
-                    if (mysqli_stmt_num_rows($stmt) == 0) {
-                        echo "<h1 style='color:red'>There are no open tickets</h1>";
-                    } else {
-                        while (mysqli_stmt_fetch($stmt)) {
-                            echo "<a href='Ticket.php?ticket=" . $TicketId . "'><div class='Ticket' id='effectlblue'>";
-                                echo "<div class='TicketLeft'>";
-                                    echo "<div class='Name'>" . $Title . "&nbsp;</div>";
-                                    if ($Status == "Open") {
-                                        $color = "Red";
-                                    } elseif ($Status == "In Progress") {
-                                        $color = "Orange";
-                                    } else {
-                                        $color = "Green";
-                                    }
-                                    echo "<div class='Status' style='background-color: " . $color . "'></div>";
-                                echo "</div>";
-                                echo "<div class='TicketRight'>";
-                                    echo "<div class='Helper'><div class='Text'>" . $AdminId . "</div>";
-                                    echo "<img id='HelperPic' src='../img/Jesus.jpg'" . " alt='userPic'>"
-                                            . "<form method='POST' action='?ticket=$TicketId' class='DropBtn'>"
-                                            . "<input class='DropBtn' type='Submit' name='Drop' value='X'>"
-                                            . "</form>"
-                                        . "</div>";
-                                    
-                                                if(isset($_POST['Drop'])){
-                                                $SQLString2 = "DELETE FROM " . $TicketTable . " WHERE $TicketTable . TicketID = " . $_GET['ticket'];
-                                                if($stmt2 = mysqli_prepare($DBConnect, $SQLString2)){
-                                                    mysqli_stmt_execute($stmt2);
-                                                    header('location: adminTickets.php');
-                                                }
-                                                mysqli_stmt_close($stmt2);
-                                            }
-                                echo "</div>";
-                            echo "</div></a>";
-                        }
-                        mysqli_stmt_close($stmt);
-                    }
-                }
-                ?>
+                <div class="Ticket" id="effectlblue">
+                    <div class="TicketLeft">
+                        <div class="Name">Tickets&nbsp;</div>
+                        <div class="Status"></div>
+                    </div>
+                    <div class="TicketRight">
+                        <div class="Helper"><div class="Text">Helper Name</div>
+                            <img id="HelperPic" src="../img/Jesus.jpg" alt="userPic"><div id="effectblue" class="Drop"><div class="Delete">X</div></div></div>
+                        
+                    </div>
+                </div>
             </div>
         </div>
     </body>
