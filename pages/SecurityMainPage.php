78115bbb57be97068723f6f860b0d97d3158ecc7
ShadowSongg <ShadowSongg@users.noreply.github.com>
2020-01-12 16:14:43 +0100

    added html and css where i could

--- pages/SecurityMainPage.php
+++ pages/SecurityMainPage.php
@@ -1,83 +1,92 @@
-<div id="fullPage">
-            <div id="header">
-				<a href='index.php?page5=SecurityMainPage.php'><img id="logoPic" src="../img/nhl.png" alt="nhl"></a>
-				<h1 id='white'>Operation Desk</h1>
-				<div id="user">
-					<img id='userPic' src="<?php echo $_SESSION['path'];  ?>" alt="userPic">
-					<p id='userName'><?php echo $_SESSION['name']; ?></p>
-					<p id='userNameLogOut'><a href="index.php?page=logout"><img src='../img/logout2.png' ></a></p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+</head>
+<body>
+		<div id="fullPage">
+				<div id="header">
+					<a href='index.php?page5=SecurityMainPage.php'><img id="logoPic" src="../img/nhl.png" alt="nhl"></a>
+					<h1 id='white'>Operation Desk</h1>
+					<div id="user">
+						<img id='userPic' src="<?php echo $_SESSION['path'];  ?>" alt="userPic">
+						<p id='userName'><?php echo $_SESSION['name']; ?></p>
+						<p id='userNameLogOut'><a href="index.php?page=logout"><img src='../img/logout2.png' ></a></p>
+					</div>
 				</div>
-			</div>
-	<?php
-		$dbName = 'helpdesk';
-		$conn = mysqli_connect("127.0.0.1", "root", "", $dbName) OR DIE ('Error');
-		if (isset($_GET['AdminID'])){
-			$idd = $_GET['AdminID'];
-			$TableName = 'Admin';
-			$query = "DELETE FROM ".$TableName." WHERE AdminID LIKE ?";
-			if ($stmt = mysqli_prepare($conn, $query)) {
-				mysqli_stmt_bind_param($stmt, 'i', $idd);
-				if(!mysqli_stmt_execute($stmt)){
-					echo 'Error100';
-				}
-			} else {
-				echo 'Error';
-			}
-			
-		} elseif(isset($_GET['UserID'])){
-			$idd = $_GET['UserID'];
-			$TableName = 'Employee';
-			$query = "DELETE FROM ".$TableName." WHERE UserID LIKE ?";
-			if ($stmt = mysqli_prepare($conn, $query)) {
-				mysqli_stmt_bind_param($stmt, 'i', $idd);
-				if(!mysqli_stmt_execute($stmt)){
-					echo 'Error100';
-				}
-			} else {
-				echo 'Error';
-			}
-		}
-	?>
-	<h1>Admins: </h1>
-	<?php 
-		$TableName = 'Admin';
-		$query = "SELECT AdminID, Email, Admin_Name, Password, Permission_Level FROM " . $TableName;
-		if($stmt = mysqli_prepare($conn, $query)){
-			if(mysqli_stmt_execute($stmt)){
-				mysqli_stmt_bind_result($stmt, $id, $email, $name, $password, $level);
-				mysqli_stmt_store_result($stmt);
-				if(mysqli_stmt_num_rows($stmt) == 0){
-					echo '<p>There are no data!</p>';
-				} else {
-					echo "<table width='100%' border='1'>";
-					echo "<tr><th>ID</th>    
-					<th>Email</th>
-					<th>Full Name</th>
-					<th>Password</th>
-					<th>Level</th>
-					<th>Update</th>
-					<th>Delete</th></tr>";
-					while(mysqli_stmt_fetch($stmt)){
-						echo "<td>".$id."</td>";
-						echo "<td>".$email."</td>";
-						echo "<td>".$name."</td>";
-						echo "<td>".$password."</td>";
-						echo "<td>".$level."</td>";
-						echo "<td><a href='index.php?page6=SecurityUpdate.php-".$id."'>Update</a></td>";
-						echo "<td><a href='index.php?AdminID=".$id."'>Delete</a></td></tr>";
+		<?php
+			$dbName = 'helpdesk';
+			$conn = mysqli_connect("127.0.0.1", "root", "", $dbName) OR DIE ('Error');
+			if (isset($_GET['AdminID'])){
+				$idd = $_GET['AdminID'];
+				$TableName = 'Admin';
+				$query = "DELETE FROM ".$TableName." WHERE AdminID LIKE ?";
+				if ($stmt = mysqli_prepare($conn, $query)) {
+					mysqli_stmt_bind_param($stmt, 'i', $idd);
+					if(!mysqli_stmt_execute($stmt)){
+						echo 'Error100';
 					}
-					echo '</table>';
+				} else {
+					echo 'Error';
+				}
+				
+			} elseif(isset($_GET['UserID'])){
+				$idd = $_GET['UserID'];
+				$TableName = 'Employee';
+				$query = "DELETE FROM ".$TableName." WHERE UserID LIKE ?";
+				if ($stmt = mysqli_prepare($conn, $query)) {
+					mysqli_stmt_bind_param($stmt, 'i', $idd);
+					if(!mysqli_stmt_execute($stmt)){
+						echo 'Error100';
+					}
+				} else {
+					echo 'Error';
+				}
+			}
+		?>
+		<h1>Admins: </h1>
+		<?php 
+			$TableName = 'Admin';
+			$query = "SELECT AdminID, Email, Admin_Name, Password, Permission_Level FROM " . $TableName;
+			if($stmt = mysqli_prepare($conn, $query)){
+				if(mysqli_stmt_execute($stmt)){
+					mysqli_stmt_bind_result($stmt, $id, $email, $name, $password, $level);
+					mysqli_stmt_store_result($stmt);
+					if(mysqli_stmt_num_rows($stmt) == 0){
+						echo '<p>There are no data!</p>';
+					} else {
+						echo "<table width='100%' border='1'>";
+						echo "<tr><th>ID</th>    
+						<th>Email</th>
+						<th>Full Name</th>
+						<th>Password</th>
+						<th>Level</th>
+						<th>Update</th>
+						<th>Delete</th></tr>";
+						while(mysqli_stmt_fetch($stmt)){
+							echo "<td>".$id."</td>";
+							echo "<td>".$email."</td>";
+							echo "<td>".$name."</td>";
+							echo "<td>".$password."</td>";
+							echo "<td>".$level."</td>";
+							echo "<td><a href='index.php?page6=SecurityUpdate.php-".$id."'>Update</a></td>";
+							echo "<td><a href='index.php?AdminID=".$id."'>Delete</a></td></tr>";
+						}
+						echo '</table>';
+					}
+				} else {
+					echo 'Error2';
 				}
 			} else {
 				echo 'Error2';
 			}
-		} else {
-			echo 'Error2';
-		}
-	?>
-	<p><a href='index.php?page5=SecurityRegister.php'>Create new Admin<a></p>
-	<h1>Users: </h1>
-	<?php 
+		?>
+		<p><a href='index.php?page5=SecurityRegister.php'>Create new Admin<a></p>
+		<h1>Users: </h1>
+		<?php 
 		$TableName = 'Employee';
 		$query = "SELECT UserID, Email, Employee_Name, Company_Name FROM " . $TableName;
 		if($stmt = mysqli_prepare($conn, $query)){
@@ -110,3 +119,5 @@
 		}
 	?>
 </div>
+</body>
+</html>
--- pages/SecurityRegister.php
+++ pages/SecurityRegister.php
@@ -1,78 +1,71 @@
-<div id="fullPage">
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="../CSS/boxstyle.css">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Security Main Page</title>
+    </head>
+    <body>
+        <div id="fullPage">
             <div id="header">
-				<a href='index.php?page5=SecurityMainPage.php'><img id="logoPic" src="../img/nhl.png" alt="nhl"></a>
-				<h1 id='white'>Operation Desk</h1>
-				<div id="user">
-					<img id='userPic' src="<?php echo $_SESSION['path'];  ?>" alt="userPic">
-					<p id='userName'><?php echo $_SESSION['name']; ?></p>
-					<p id='userNameLogOut'><a href="index.php?page=logout"><img src='../img/logout2.png' ></a></p>
-				</div>
-			</div>
-<form method="post" enctype="multipart/form-data">
-    <input type="text" name="firstname" placeholder="First Name" pattern="[a-zA-Z']*">
-    <input type="text" name="lastname" placeholder="Last Name" pattern="[a-zA-Z']*">
-    <input type="email" name="email" placeholder="Email">
-    <input type="text" name="level" placeholder="Level of Rights">
-    <input type="password" name="pw" placeholder="Password">
-    <input type="password" name="pwr" placeholder="Repeat Password">
-	<div id='bla'>
-		<img id="blah" src="#"/>
-	</div>
-	<input id='choose' type='file' name="photo" onchange="readURL(this);">
-        <?php
-            if(isset($_POST['register'])){
-				$dbName = 'helpdesk';
-				$conn = mysqli_connect("127.0.0.1", "root", "", $dbName) OR DIE ('Error');
-					If(empty($_POST['email']) OR empty($_POST['pw']) OR empty($_POST['pwr']) 
-					OR empty($_POST['firstname']) OR empty($_POST['lastname']) OR empty($_POST['level'])){
-					  echo "<p>Please fill all empty space!</p>";
-					} else {
-						If($_POST['pw'] == $_POST['pwr']){
-							$TableName = "Admin";
-							$fname = $_POST['firstname'];
-							$lname = $_POST['lastname'];
-							$name = $fname.' '.$lname;
-							$password_hash = password_hash($_POST['pw'], PASSWORD_DEFAULT);
-							$email = $_POST['email'];
-							$level = $_POST['level'];
-							if (!file_exists('../avatar/'.$fname.'-'.$lname)) {
-								mkdir('../avatar/'.$fname.'-'.$lname, 0777, true);
-							}
-							if ((($_FILES["photo"]["type"] == "image/png") OR ($_FILES["photo"]["type"] == "image/jpeg") OR ($_FILES["photo"]["type"] == "image/jpg"))
-							AND ($_FILES["photo"]["size"] < 600000)){
-								if ($_FILES["photo"]["error"] > 0){
-									echo "<p class='red'>Error!</p>";
-								} else {
-									if (file_exists('../avatar/'.$fname.'-'.$lname.'/'.$_FILES["photo"]["name"])){
-										echo $_FILES["photo"]["name"] . " already exists. ";
-									} else {
-										move_uploaded_file($_FILES["photo"]["tmp_name"], '../avatar/'.$fname.'-'.$lname.'/'.$_FILES["photo"]["name"]);
-										$path = '../avatar/'.$fname.'-'.$lname.'/'.$_FILES["photo"]["name"];
-										$query = "INSERT INTO ". $TableName ." VALUES(NULL,?,?,?,?,?)";
-										If($stmt = mysqli_prepare($conn, $query)){
-											mysqli_stmt_bind_param($stmt, 'sssss', $email, $name, $password_hash, $level, $path);
-											If(mysqli_stmt_execute($stmt)){
-												echo '<p>New admin was created!</p>';
-											} else {
-												echo "Data has not been inserted";
-											}
-											mysqli_stmt_close($stmt);
-										} else {
-											echo '<p>Error!</p>';
-										}
-									}
-								}
-							} else {
-								echo '<p class="red">Invalid file!</p>';
-							}
-						} else {
-							echo '<p>Passwords are not the same!</p>';
-						}
-					}
-				mysqli_close($conn);
-				} 
-			
-        ?>
-    <input class="inputbtn" type="submit" name="register" value="Register">
-</form>
-</div>
+                <a href="SecurityMainPage.php"><img id="logoPic" src="../img/nhl.png" alt="nhl"></a>
+                <img id="userPic" src="../img/Jesus.jpg" alt="userPic">
+            </div>
+        </div>
+ <a href='index.php?page=logout'><div  class="logout"> <p>Log out</p> </div></a>
+        <div> 
+           
+            <form  id="skrREG" method="post">
+                <input  class="skrInput" type="text" name="firstname" placeholder="First Name" pattern="[a-zA-Z']*">
+                <input  class="skrInput" type="text" name="lastname" placeholder="Last Name" pattern="[a-zA-Z']*">
+                <input  class="skrInput" type="email" name="email" placeholder="Email">
+                <input  class="skrInput" type="text" name="level" placeholder="Level of Rights">
+                <input  class="skrInput" type="password" name="pw" placeholder="Password">
+                <input  class="skrInput" type="password" name="pwr" placeholder="Repeat Password">
+
+                <?php
+                if (isset($_POST['register'])) {
+                    $dbName = 'helpdesk';
+                    $conn = mysqli_connect("127.0.0.1", "root", "", $dbName) OR DIE('Error');
+                    if (mysqli_select_db($conn, $DBName)) {
+                        If (empty($_POST['email']) OR empty($_POST['pw']) OR empty($_POST['pwr'])
+                                OR empty($_POST['firstname']) OR empty($_POST['lastname']) OR empty($_POST['level'])) {
+                            echo "<p>Please fill all empty space!</p>";
+                        } else {
+                            If ($_POST['pw'] == $_POST['pwr']) {
+                                $TableName = "Admin";
+                                $name = $_POST['firstname'] . ' ' . $_POST['lastname'];
+                                $password_hash = password_hash($_POST['pw'], PASSWORD_DEFAULT);
+                                $email = $_POST['email'];
+                                $level = $_POST['level'];
+                                $query = "INSERT INTO " . $TableName . " VALUES(NULL,?,?,?,?)";
+                                If ($stmt = mysqli_prepare($conn, $query)) {
+                                    mysqli_stmt_bind_param($stmt, 'ssss', $email, $name, $password_hash, $level);
+                                    If (mysqli_stmt_execute($stmt)) {
+                                        echo '<p>New admin was created!</p>';
+                                    } else {
+                                        echo "Data has not been inserted";
+                                    }
+                                    mysqli_stmt_close($stmt);
+                                } else {
+                                    echo '<p>Error!</p>';
+                                }
+                            } else {
+                                echo '<p>Passwords are not the same!</p>';
+                            }
+                        }
+                    } else {
+                        die(mysqli_error($conn));
+                    }
+                    mysqli_close($conn);
+                }
+                ?>
+                <input class="skrInput" type="submit" name="register" value="Register">
+            </form>
+        </div>
+        <a href='index.php?page5=SecurityMainPage.php'><div class="logout"><p>Main Page</p></div></a>
+
+    </body>
+</html>
