c5bd2dd1c6f86001575c6e3c59610c4463783531
KBG360 <KBG360@users.noreply.github.com>
2020-01-14 13:25:16 +0100

    Konstan | idk man

--- pages/Faq.php
+++ pages/Faq.php
@@ -10,6 +10,23 @@
 </head>
 
 <body>
+    <script>
+        var coll = document.getElementsByClassName("collapsible");
+        var i;
+
+        for (i = 0; i < coll.length; i++) {
+            coll[i].addEventListener("click", function() {
+                this.classList.toggle("active");
+                var content = this.nextElementSibling;
+                if (content.style.maxHeight) {
+                    content.style.maxHeight = null;
+                } else {
+                    content.style.maxHeight = content.scrollHeight + "px";
+                }
+            });
+        }
+    </script>
+
     <div id="fullPage">
         <div id="header">
             <a href='index.php'><img id="logoPic" src="../img/nhl.png" alt="nhl"></a>
@@ -18,14 +35,17 @@
                 <h1 id='userName'><?php echo $_SESSION['name']; ?></h1>
             </div> -->
         </div>
-        
+
         <div id="body-space">
             <div id="faq-header">
                 <h1 id="faq-letters">Frequently Asked Questions</h1>
             </div>
 
             <div id="question-section">
-                
+                <button class="collapsible">Open Collapsible</button>
+                <div class="content">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
             </div>
         </div>
 </body>
